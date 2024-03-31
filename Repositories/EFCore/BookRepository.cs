8219511269805235a8708cd17846820947fc7f22
safakcivelek <safakcivelek3@gmail.com>
2024-03-31 11:24:48 +0300

    Create RepositoryManager and ServiceExtensions for enhanced modularity and update dependencies accordingly

--- Repositories/EFCore/BookRepository.cs
+++ Repositories/EFCore/BookRepository.cs
@@ -16,11 +16,16 @@ namespace Repositories.EFCore
 
         public void CreateOneBook(Book book) => Create(book);
         public void DeleteOneBook(Book book) => Delete(book);
+
         public IQueryable<Book> GetAllBooks(bool trackChanges) =>
             FindAll(trackChanges)
             .OrderBy(b => b.Id);
-        public IQueryable<Book> GetOneBookById(int id, bool trackChanges) =>
-            FindByCondition(b => b.Id.Equals(id), trackChanges);
+
+        public Book GetOneBookById(int id, bool trackChanges) =>
+             FindByCondition(b => b.Id.Equals(id), trackChanges)
+             .SingleOrDefault();
+
         public void UpdateOneBook(Book book) => Update(book);
+
     }
 }
