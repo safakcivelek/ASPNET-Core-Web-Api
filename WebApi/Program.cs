93863b8cb65ca0d2661e1e01268771edb919aa9f
safakcivelek <safakcivelek3@gmail.com>
2024-04-01 14:13:43 +0300

    Implement logging infrastructure with NLog - Add NLog.Extensions.Logging package for enhanced logging capabilities - Create LoggerService as a custom logging service abstraction - Implement LoggerManager as the concrete implementation of ILoggerService - Add nlog.config file for NLog configuration settings - Register logging services in the DI container for application-wide use

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using NLog;
 using Repositories.EFCore;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
@@ -14,6 +18,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
