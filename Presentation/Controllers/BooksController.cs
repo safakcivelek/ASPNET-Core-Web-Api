e366ef0c07ca0ac1f2f3883b1765aec88d143e60
safakcivelek <safakcivelek3@gmail.com>
2024-04-16 21:03:40 +0300

    Add RootController and update media type configurations in ServiceExtensions
    - Introduced RootController to provide a root entry point for API navigation with support for custom media types 'application/vnd.btkakademi.apiroot'.
    - Extended custom media type support in MvcOptions to include 'application/vnd.btkakademi.apiroot+json' and 'application/vnd.btkakademi.apiroot+xml'.
    - Enhanced service configuration for CORS, logging, and media types to improve API flexibility and client integration.

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -55,7 +55,7 @@ namespace Presentation.Controllers
         }
 
         [ServiceFilter(typeof(ValidationFilterAttribute))]
-        [HttpPost]
+        [HttpPost(Name = "CreateOneBookAsync")]
         public async Task<IActionResult> CreateOneBookAsync([FromBody] BookDtoForInsertion bookDto)
         {
             var book = await _manager.BookService.CreateOneBookAsync(bookDto);
