99920a30495aa9ba89b3634bfca037164b3667ea
safakcivelek <safakcivelek3@gmail.com>
2024-04-20 23:29:38 +0300

    Update RepositoryContext to inherit from IdentityDbContext<User>

--- Repositories/EFCore/RepositoryContext.cs
+++ Repositories/EFCore/RepositoryContext.cs
@@ -1,15 +1,11 @@
 ﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repositories.EFCore.Config;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories.EFCore
 {
-    public class RepositoryContext : DbContext
+    public class RepositoryContext : IdentityDbContext<User>
     {
         public RepositoryContext(DbContextOptions options) : base(options)
         {
@@ -19,6 +15,7 @@ namespace Repositories.EFCore
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new BookConfig());
         }
     }
