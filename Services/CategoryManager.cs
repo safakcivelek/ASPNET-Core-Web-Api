fd2e391d5efc88a148abb7b27ef73c5ec7488fa7
safakcivelek <safakcivelek3@gmail.com>
2024-05-03 10:21:44 +0300

    ICategoryService and CategoryManager created

---
+++ Services/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly IRepositoryManager _manager;
+
+        public CategoryManager(IRepositoryManager manager)
+        {
+            _manager = manager;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges)
+        {
+            return await _manager.Category.GetAllCategoriesAsync(trackChanges);
+        }
+
+        public async Task<Category> GetOnCategoryByIdAsync(int id, bool trackChanges)
+        {
+            return await _manager.Category.GetOneCategoryByIdAsync(id, trackChanges);
+        }
+    }
+}
