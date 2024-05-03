8aeba9beed1c3afd80185608e151641fd147da6a
safakcivelek <safakcivelek3@gmail.com>
2024-05-03 14:07:27 +0300

    Migration : "Establish one-to-many relationship between Category and Book entities and apply migration to update database schema."

---
+++ WebApi/Migrations/20240503110059_createRelationBetweenBookAndCategory.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class createRelationBetweenBookAndCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cbc8b646-418f-4e4b-ada3-0b2c95f3b580");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dde403fd-39f2-4349-9f20-7f298372c8a3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fa7735c1-1b97-44db-bc35-ae62be59f4ed");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "355bbab2-c1b9-4b44-897b-71187fb3687d", "49fb3ced-400d-4938-8fe4-4c1fff576ec2", "Editor", "EDITOR" },
+                    { "52593f7a-9f65-46ee-b834-56779bc3eb98", "d5995941-8bdb-4267-9992-a14a8466dd51", "User", "USER" },
+                    { "7915218d-8b98-44e2-bc8b-e0f6bde2f6bb", "feea6dfe-878f-4429-b1f0-c57a45e6965e", "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Categories_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Categories_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "355bbab2-c1b9-4b44-897b-71187fb3687d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "52593f7a-9f65-46ee-b834-56779bc3eb98");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7915218d-8b98-44e2-bc8b-e0f6bde2f6bb");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Books");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "cbc8b646-418f-4e4b-ada3-0b2c95f3b580", "997b036c-95bf-4175-9618-6a74bb99800b", "Editor", "EDITOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "dde403fd-39f2-4349-9f20-7f298372c8a3", "27ae3b20-f34c-499d-aed4-03ab3c4a97e5", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "fa7735c1-1b97-44db-bc35-ae62be59f4ed", "79c7212c-96fe-4a53-a400-f5f548c89d4d", "User", "USER" });
+        }
+    }
+}
