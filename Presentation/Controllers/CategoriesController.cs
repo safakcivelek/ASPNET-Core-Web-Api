21287894fcde9af13d368f7d72707691dd8b8ade
safakcivelek <safakcivelek3@gmail.com>
2024-05-03 13:39:29 +0300

    "Add CategoriesController to handle category-related API endpoints."

---
+++ Presentation/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IServiceManager _services;
+
+        public CategoriesController(IServiceManager services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategoriesAsync()
+        {
+            return Ok(await _services.CategoryService.GetAllCategoriesAsync(false));
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOneCategoryAsync([FromRoute] int id)
+        {
+            return Ok(await _services.CategoryService.GetOnCategoryByIdAsync(id,false));
+        }
+    }
+}
