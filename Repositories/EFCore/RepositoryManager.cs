e80c5bb37957d245e00821dde062e930e7afc4a5
safakcivelek <safakcivelek3@gmail.com>
2024-05-03 08:07:18 +0300

    Updated RepositoryManager to include CategoryRepository.

--- Repositories/EFCore/RepositoryManager.cs
+++ Repositories/EFCore/RepositoryManager.cs
@@ -11,18 +11,24 @@ namespace Repositories.EFCore
     {
         private readonly RepositoryContext _context;
         private readonly Lazy<IBookRepository> _bookRepository;
+        private readonly Lazy<ICategoryRepository> _categoryRepository;
 
         public RepositoryManager(RepositoryContext context)
         {
             _context = context;
             _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(_context));
+            _categoryRepository = new Lazy<ICategoryRepository>(() => new CategoryRepository(_context));
         }
 
         public IBookRepository Book => _bookRepository.Value;
+        public ICategoryRepository Category => _categoryRepository.Value;
 
         public async Task SaveAsync()
         {
            await _context.SaveChangesAsync();
         }
+
+        // Tüm kaynakların (Book,Category vb.) repo tanımları için IoC'ye kayıt yapmak istemediğim için , dolayısıyla tek new'leme işlemini burada gerçekleştiriyorum.
+        // Yani RepositoryManager constructor'ından çıkmadan benim bir CategoryRepository'yi somut hale getirmiş olmam lazım, dolayısıylada o işlemide burada gerçekleştirmiş oldum.
     }
 }
