fbf3767f6b3c4c7cccb66c9748b73a1ec66c732c
safakcivelek <safakcivelek3@gmail.com>
2024-05-04 22:18:38 +0300

    File upload : Implement file upload functionality in FilesController - This update introduces basic file handling capabilities to the API, allowing for file uploads to a designated folder on the server.

---
+++ Presentation/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            
+            // folder
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Media");
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            // path
+            var path = Path.Combine(folder, file?.FileName);
+
+            // stream
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // response body
+            return Ok(new
+            {
+                file = file.FileName,
+                path = path,
+                size = file.Length
+            });
+        }
+    }
+}
